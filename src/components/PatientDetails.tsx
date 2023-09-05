65a8c718ba2222bd573f0e2faf58392d13ea78f4
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-05 21:33:22 +0200

    ex 9.23 done, add frontend functionality to show further details about each patient, taken from new backend data

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -15,6 +15,7 @@ const PatientDetails = () => {
         if (id) {
           const fetchedPatient = await patientService.getPatientDetails(id);
           console.log("patient details", fetchedPatient);
+          console.log("patient entries", fetchedPatient.entries);
           setPatient(fetchedPatient);
         }
       } catch (error) {
@@ -28,13 +29,26 @@ const PatientDetails = () => {
     <div>
       {patient ? (
         <>
-          <h3>
+          <h2>
             {patient.name}
             {patient.gender === "female" ? <FemaleIcon /> : <MaleIcon />}
-          </h3>
+          </h2>
           <p>date of birth: {patient.dateOfBirth}</p>
           <p>ssn: {patient.ssn}</p>
           <p>occupation: {patient.occupation}</p>
+          <h3>entries</h3>
+          {patient.entries.map((p) => (
+            <div key={p.id}>
+              <p>
+                {p.date} {p.description}
+              </p>
+              <ul>
+                {p.diagnosisCodes?.map((d) => (
+                  <li key={d}>{d}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </>
       ) : (
         <p>Loading patient information...</p>
--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,7 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
 
 export interface Patient {
@@ -17,6 +17,49 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
+
+export enum HealthCheckRating {
+  "Health" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
