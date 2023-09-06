f367347677ed20910900e2c97be2bb330f455a27
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-06 12:52:26 +0200

    ex 9.25 done, style all components for displaying patient data, use material UI, use typescript annotation for checking passed data

--- src/components/EntryDetails/HospitalEntry.tsx
+++ src/components/EntryDetails/HospitalEntry.tsx
@@ -1,11 +1,28 @@
 import { HospitalEntry } from "../../types";
+import { Paper, Box } from "@mui/material";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
 
-const HospitalEntryComponent: React.FC<{ entry: HospitalEntry }> = ({ entry }) => {
+const HospitalEntryComponent: React.FC<{ entry: HospitalEntry }> = ({
+  entry,
+}) => {
   console.log("hospital entry", entry);
   return (
-    <div>
-      {entry.date} {entry.description} {entry.discharge.criteria}
-    </div>
+    <Box
+      border={1}
+      borderRadius={1}
+      borderColor="primary.main"
+      marginBottom={2}
+    >
+      <Paper style={{ padding: "16px" }}>
+        <p>
+          {entry.date} <LocalHospitalIcon />
+        </p>
+        <p>
+          <i>{entry.description}</i>
+        </p>
+        <p>diagnosed by {entry.specialist}</p>
+      </Paper>
+    </Box>
   );
 };
 
