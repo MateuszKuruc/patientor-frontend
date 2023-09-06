c38d342d6fba677421043c2afd41835c5da6862a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-06 12:41:09 +0200

    add styling for healtcheck component using material UI

--- src/components/EntryDetails/HealtcheckEntry.tsx
+++ src/components/EntryDetails/HealtcheckEntry.tsx
@@ -1,22 +1,42 @@
 import { HealthCheckEntry } from "../../types";
 import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Paper, Box } from "@mui/material";
 
 const HealthcheckComponent: React.FC<{ entry: HealthCheckEntry }> = ({
   entry,
 }) => {
-  //   const healtCheckRating = entry.healthCheckRating;
-  // const getIconColor = () => {
-  //     switch(entry.)
-  // }
-  return (
-    <div>
-      <p>
-        {entry.date} <HealthAndSafetyIcon />
-      </p>
+  const getIconColor = () => {
+    switch (entry.healthCheckRating) {
+      case 0:
+        return "green";
+      case 1:
+        return "gold";
+      case 2:
+        return "orange";
+      case 3:
+        return "red";
+      default:
+        return "black";
+    }
+  };
 
-      {entry.description}
-    </div>
+  const iconColor = getIconColor();
+
+  return (
+    <Box border={1} borderColor="primary.main" borderRadius={1}>
+      <Paper style={{ padding: "16px" }}>
+        <p>
+          {entry.date} <HealthAndSafetyIcon />
+        </p>
+        <p>
+          Health Rating: <FavoriteIcon style={{ color: iconColor }} />
+        </p>
+        <p>
+          <i>{entry.description}</i>
+        </p>
+      </Paper>
+    </Box>
   );
 };
 
